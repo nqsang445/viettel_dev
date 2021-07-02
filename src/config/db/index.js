d4a7d044eae8d9b65ad26dad87dff4f47bc396f8
nqsang445 <83231121+nqsang445@users.noreply.github.com>
2021-07-02 09:31:37 +0700

    update header phone 2

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 
 async function connect() {
     try {
-        await mongoose.connect('mongodb://localhost:27017/viettel_web_dev', {
+        await mongoose.connect('mongodb+srv://viettel_web_dev:0345617864@cluster0.uovjr.mongodb.net/viettel_web_dev?retryWrites=true&w=majority', {
             useNewUrlParser: true,
             useUnifiedTopology: true,
             useFindAndModify: false,
